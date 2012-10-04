13e1ab15f546b8f46807675e74358701595817ec
koogawa <koogawa.app@gmail.com>
2012-10-04 13:14:55 +0900

    Initial Commit

---
+++ GeocodingDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GeocodingDemo
+//
+//  Created by koogawa on 2012/10/04.
+//  Copyright (c) 2012年 koogawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
